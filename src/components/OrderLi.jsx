e3be43637f7cc1edfa987c09bc2d527e703cd649
paczek07 <jesmdz@gmail.com>
2022-05-30 23:05:07 +0300

    Large format Home implementation

--- src/components/OrderLi.jsx
+++ src/components/OrderLi.jsx
@@ -6,9 +6,9 @@ const OrderLi = () => {
     <li>
       <Link to='/pickup'>
         <div className='flex justify-between p-4 bg-white_bg text-sm font-light tracking-wide text-light_font hover:bg-dark_purple hover:text-white_bg'>
-          <h1 className=''>id: 324yyas87y213ugy</h1>
+          <h1 className='overflow-hidden'>id: 324yyas87y213ugy</h1>
           <h1> items: 11</h1>
-          <h1>22/05/2022</h1>
+          <h1>Date: 22/05/2022</h1>
         </div>
       </Link>
 
